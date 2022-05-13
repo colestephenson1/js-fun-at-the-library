b525a49cf1414eefa05265c13c9d5791d30827b0
Cole Stephenson <colestephenson94@gmail.com>
2022-05-12 19:14:44 -0600

    finish first 2 q's of books.js

--- src/book.js
+++ src/book.js
@@ -1,11 +1,25 @@
-function createTitle() {
-  
-}
+function createTitle(bookIdea) {
+return `The ${bookIdea}`
+};
+
+
+function buildMainCharacter(name, age, pronouns) {
+return {
+  name: name,
+  age: age,
+  pronouns: pronouns
+};
+};
+
+function saveReview(reviews, movieRevs) {
+return movieRevs.push(reviews)
+
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
