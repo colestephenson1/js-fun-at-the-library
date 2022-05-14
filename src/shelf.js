83267c7af859368977e4ed6867fa9f0ba008a5fb
Cole Stephenson <colestephenson94@gmail.com>
2022-05-14 14:38:46 -0600

    Finish shelves

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
+//shelfBook
+
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    var shelvedBook = shelf.unshift(book)
+    return shelvedBook
+};
+};
+
+//unshelfBook
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++ ) {
+if (shelf[i].title === book) {
+return shelf.splice(i, 1)
+};
+};
+};
+
+//listTitles
+
+function listTitles(fantasyShelf) {
+return `${fantasyShelf[0].title}, ${fantasyShelf[1].title}, ${fantasyShelf[2].title}`
+};
+
+//searchShelf
+
+function searchShelf(shelf, name) {
+  for (i = 1; i < shelf.length; i--) {
+  if (shelf[i].title === name) {
+return true;
+} else {
+  return false
+};
+};
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
